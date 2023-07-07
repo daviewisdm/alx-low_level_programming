59975c88db8d58c0a5145a16cd901117d52bf13e
Davie Kibet <wisdomdavie24@gmail.com>
2023-07-07 00:11:09 -0700

    A library is not a luxury but one of the necessities of life

---
+++ 0x09-static_libraries/2-strlen.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _strlen - Checks on the length of the string
+ * @s: string to be checked
+ * Return: length of the string
+ */
+
+int _strlen(char *s)
+{
+	int len = 0;
+
+	while (*s != '\0')
+	{
+		len++;
+		s++;
+	}
+	return (len);
+}
