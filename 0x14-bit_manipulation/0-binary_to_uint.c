c4a597d07056304c1b8880ce1456296e32738bb5
Davie Kibet <118133684+daviewisdm@users.noreply.github.com>
2023-08-03 13:32:52 +0300

    Update 0-binary_to_uint.c

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -6,20 +6,42 @@
  *
  * Return: the converted number
  */
+
 unsigned int binary_to_uint(const char *b)
 {
-	int p;
-	unsigned int dec_val = 0;
-
+	int i, usigned_int = 0, dec_val = 1;
+	/* if we have no string return 0 */
 	if (!b)
 		return (0);
 
-	for (p = 0; b[p]; i++)
+	/* loop from end of string (-1 to ignore '\0' ) to start of string */
+	for (i = _strlen(b) - 1; i >= 0; i--)
 	{
-		if (b[p] < '0' || b[p] > '1')
+		if (b[i] != '0' && b[i] != '1')
+		{
 			return (0);
-		dec_val = 2 * dec_val + (b[p] - '0');
+		}
+		if (b[i] == '1')
+		{
+			usigned_int += dec_val;
+		}
+		dec_val *= 2;
 	}
+	return (usigned_int);
+}
 
-	return (dec_val);
+/**
+ * _strlen - Function that returns the length of string.
+ * @s: pointer to the null-terminated byte string to be examined.
+ *
+ * Return: The length of the null-terminated byte string s.
+ */
+
+int _strlen(const char *s)
+{
+	int len = 0;
+
+	while (s[len] != '\0')
+		len++;
+	return (len);
 }
