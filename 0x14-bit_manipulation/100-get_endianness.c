b38acdb9bcc676276590011600c667a06f2f37f1
Davie Kibet <118133684+daviewisdm@users.noreply.github.com>
2023-08-03 13:58:54 +0300

    Update 100-get_endianness.c

--- 0x14-bit_manipulation/100-get_endianness.c
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -1,22 +1,22 @@
 #include "main.h"
 
 /**
- * get_endianness - function that checks the endianness.
+ * get_endianness - a function that checks the endianness.
  *
- * Return: 0 if big endian, 1 if little endian.
+ * Return: 0 if big-endian, 1 if little-endian.
  */
 
 int get_endianness(void)
 {
 	unsigned int n = 1; /* 00 00 00 01 */
-	char *c = (char *) &n;
+	char *x = (char *) &n;
 
 	/**
 	 * if little endian, n will be stored in memory as
-	 * 01 00 00 00, so *c = 1
+	 * 01 00 00 00, so *x = 1
 	 */
-	if (*c)
+	if (*x)
 		return (1);
-	/* otherwise return 0 meaning it is big endian */
+	/* otherwise return 0 meaning it is big-endian */
 	return (0);
 }
