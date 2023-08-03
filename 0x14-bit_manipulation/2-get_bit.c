b2b8394317265857639c45f7827a800e09353133
Davie Kibet <118133684+daviewisdm@users.noreply.github.com>
2023-08-03 13:41:55 +0300

    Update 2-get_bit.c

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -4,17 +4,16 @@
  * get_bit - returns the value of a bit at an index in a decimal number
  * @n: number to search
  * @index: index of the bit
- *
  * Return: value of the bit
  */
-int get_bit(unsigned long int n, unsigned int index)
+int get_bit(unsigned long int p, unsigned int index)
 {
 	int bit_val;
 
 	if (index > 63)
 		return (-1);
 
-	bit_val = (n >> index) & 1;
+	bit_val = (p >> index) & 1;
 
 	return (bit_val);
 }
