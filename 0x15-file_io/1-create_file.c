4db1484373f8ee158ae3b0df0ec548f49c71fec0
Davie Kibet <118133684+daviewisdm@users.noreply.github.com>
2023-08-08 14:18:08 +0300

    Update 1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -11,24 +11,24 @@
  */
 int create_file(const char *filename, char *text_content)
 {
-	int fd, w, len = 0;
+	int fwd, r, length = 0;
 
 	if (filename == NULL)
 		return (-1);
 
 	if (text_content != NULL)
 	{
-		for (len = 0; text_content[len];)
-			len++;
+		for (length = 0; text_content[length];)
+			length++;
 	}
 
-	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
-	w = write(fd, text_content, len);
+	fwd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+	r = write(fwd, text_content, length);
 
-	if (fd == -1 || w == -1)
+	if (fwd == -1 || r == -1)
 		return (-1);
 
-	close(fd);
+	close(fwd);
 
 	return (1);
 }
