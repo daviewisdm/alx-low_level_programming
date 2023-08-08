89167bec6539264532c9e0108ecf222ce1dfd302
Davie Kibet <118133684+daviewisdm@users.noreply.github.com>
2023-08-08 14:15:38 +0300

    Update 2-append_text_to_file.c

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -11,8 +11,8 @@
 int append_text_to_file(const char *filename, char *text_content)
 {
 	int fd;
-	int nletters;
-	int rwr;
+	int xletters;
+	int rw;
 
 	if (!filename)
 		return (-1);
@@ -24,12 +24,12 @@ int append_text_to_file(const char *filename, char *text_content)
 
 	if (text_content)
 	{
-		for (nletters = 0; text_content[nletters]; nletters++)
+		for (xletters = 0; text_content[xletters]; xletters++)
 			;
 
-		rwr = write(fd, text_content, nletters);
+		rw = write(fd, text_content, xletters);
 
-		if (rwr == -1)
+		if (rw == -1)
 			return (-1);
 	}
 
