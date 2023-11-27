c163b11d40d81802d0a57226145f30979877cfc0
daviewisdm <wisdomdavie@gmail.com>
2023-11-27 01:25:19 -0800

    File descriptors and permissions$

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include "main.h"
+
+int create_file(const char *filename, char *text_content) {
+   FILE *file;
+
+   if (filename == NULL) {
+       return -1;
+   }
+
+   file = fopen(filename, "w");
+
+   if (file == NULL) {
+       return -1;
+   }
+
+   if (text_content != NULL) {
+       if (fputs(text_content, file) == EOF) {
+           fclose(file);
+           return -1;
+       }
+   }
+
+   fclose(file);
+
+   return 1;
+}
