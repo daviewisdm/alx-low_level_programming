4e928105bbe22c8a2166f476d28b3882ce738125
daviewisdm <wisdomdavie@gmail.com>
2023-11-27 02:10:24 -0800

    Tread lightly, she is near

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,27 +1,30 @@
 #include <stdio.h>
 #include "main.h"
 
-int create_file(const char *filename, char *text_content) {
-   FILE *file;
+int create_file(const char *filename, char *text_content)
+{
+	FILE *file;
 
-   if (filename == NULL) {
-       return -1;
-   }
+	if (filename == NULL)
+	{
+		return (-1);
+	}
+	file = fopen(filename, "w");
 
-   file = fopen(filename, "w");
-
-   if (file == NULL) {
-       return -1;
-   }
-
-   if (text_content != NULL) {
-       if (fputs(text_content, file) == EOF) {
-           fclose(file);
-           return -1;
+       if (file == NULL)
+       {
+	       return (-1);
        }
-   }
 
-   fclose(file);
+       if (text_content != NULL)
+       {
+	       if (fputs(text_content, file) == EOF)
+	       {
+		       fclose(file);
+		       return (-1);
+	       }
+       }
+       fclose(file);
 
-   return 1;
+       return (1);
 }
